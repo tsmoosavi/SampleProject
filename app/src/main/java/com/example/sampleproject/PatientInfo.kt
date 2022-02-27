de17d6654115020ab73e808f3c13ea603f8aeca6
ts moosavi <tt.musavi@gmail.com>
2022-02-27 21:15:47 +0330

    shared pref added and visibility of button set

--- app/src/main/java/com/example/sampleproject/PatientInfo.kt
+++ app/src/main/java/com/example/sampleproject/PatientInfo.kt
@@ -1,7 +1,10 @@
 package com.example.sampleproject
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.sampleproject.databinding.ActivityPatientInfoBinding
 
@@ -12,6 +15,7 @@ class PatientInfo : AppCompatActivity() {
         binding = ActivityPatientInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViews()
+        collectPatientData()
 
     }
 
@@ -22,10 +26,36 @@ class PatientInfo : AppCompatActivity() {
         var myDoctor = Hospital.getDoctor(id)
         binding.myDoctorMassage.text = "دکتر ${myDoctor?.name} با شما تماس خواهد گرفت. "
         binding.submit.isEnabled = myDoctor?.onlineStatus == OnlineStatus.online
+        if (!saveName().isNullOrBlank()){
+            binding.patientName.visibility = View.GONE
+        }
+        if (!savePhone().isNullOrBlank()){
+            binding.patientPhone.visibility =View.GONE
+        }
+    }
+
+    private fun collectPatientData() {
         binding.submit.setOnClickListener{
             Toast.makeText(this, "اطلاعات شما ثبت شد.", Toast.LENGTH_SHORT).show()
-        }
-
-
+            var userName = binding.patientName.text.toString()
+            var userPhone = binding.patientPhone.text.toString()
+            var dataCollection : SharedPreferences = getSharedPreferences("kotlinStorage", Context.MODE_PRIVATE)
+            var editor = dataCollection.edit()
+            editor.putString("name",userName)
+            editor.putString("Phone",userPhone)
+            editor.apply()
+            var name = dataCollection.getString("name","")
+            Toast.makeText(this, "$name عزیز، اطلاعات شما ثبت شد.", Toast.LENGTH_SHORT).show()
+    }
+    }
+    fun saveName():String?{
+        var dataCollection : SharedPreferences = getSharedPreferences("kotlinStorage", Context.MODE_PRIVATE)
+        var name =  dataCollection.getString("name","")
+        return name
+    }
+    fun savePhone():String?{
+        var dataCollection : SharedPreferences = getSharedPreferences("kotlinStorage", Context.MODE_PRIVATE)
+        var phone =  dataCollection.getString("Phone","")
+        return phone
     }
 }
