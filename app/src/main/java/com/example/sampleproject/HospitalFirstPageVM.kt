bff3a59ae86114457988336d16eea583a9750415
ts moosavi <tt.musavi@gmail.com>
2022-03-14 14:45:56 +0330

    init deleted from HospitalFirstPageVM

--- app/src/main/java/com/example/sampleproject/HospitalFirstPageVM.kt
+++ app/src/main/java/com/example/sampleproject/HospitalFirstPageVM.kt
@@ -3,10 +3,11 @@ package com.example.sampleproject
 import androidx.lifecycle.ViewModel
 
 class HospitalFirstPageVM:ViewModel() {
-    var doctorList  =  arrayListOf<Doctor>()
-    init {
-        Hospital.setTestData()
-        doctorList = Hospital.doctorList
-    }
+    var doctorList  =  Hospital.doctorList
+//    var doctorList  =  arrayListOf<Doctor>()
+//    init {
+//        Hospital.setTestData()
+//        doctorList = Hospital.doctorList
+//    }
 
 }
