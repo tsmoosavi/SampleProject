5e956139547e0eb1badbc3a8ad602716e887dc07
ts moosavi <tt.musavi@gmail.com>
2022-04-21 17:56:59 +0430

    a function added for setting data on recycler view

--- app/src/main/java/com/example/sampleproject/DoctorListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorListRecyclerViewAdapter.kt
@@ -10,15 +10,23 @@ import androidx.recyclerview.widget.RecyclerView
 class DoctorListRecyclerViewAdapter(val dataSet: List<Doctor>,var clickHandler:clickHandler):
     RecyclerView.Adapter<DoctorListRecyclerViewAdapter.ViewHolder>()  {
     class  ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        var doctorId:TextView
-        var doctorName:TextView
-        var doctorField:TextView
-        init {
-            // Define click listener for the ViewHolder's View.
-            doctorId = view.findViewById<TextView>(R.id.doctorId)
-             doctorName = view.findViewById<TextView>(R.id.doctorName)
-             doctorField = view.findViewById<TextView>(R.id.doctorField)
-
+        var doctorId = view.findViewById<TextView>(R.id.doctorId)
+        var doctorName = view.findViewById<TextView>(R.id.doctorName)
+        var doctorField = view.findViewById<TextView>(R.id.doctorField)
+//        init {
+//            // Define click listener for the ViewHolder's View.
+//            doctorId = view.findViewById<TextView>(R.id.doctorId)
+//             doctorName = view.findViewById<TextView>(R.id.doctorName)
+//             doctorField = view.findViewById<TextView>(R.id.doctorField)
+//
+//        }
+        fun bind(doctor:Doctor, clickHandler:clickHandler?){
+            doctorId.text = doctor.id.toString()
+            doctorName.text = doctor.name
+            doctorField.text = doctor.field
+            doctorName.setOnClickListener{
+                 clickHandler?.invoke(doctor)
+    }
         }
     }
 
@@ -30,12 +38,13 @@ class DoctorListRecyclerViewAdapter(val dataSet: List<Doctor>,var clickHandler:c
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.doctorId.text = dataSet[position].id.toString()
-        holder.doctorName.text = dataSet[position].name
-        holder.doctorField.text = dataSet[position].field
-        holder.doctorName.setOnClickListener{
-            clickHandler?.invoke(dataSet[position])
-        }
+//        holder.doctorId.text = dataSet[position].id.toString()
+//        holder.doctorName.text = dataSet[position].name
+//        holder.doctorField.text = dataSet[position].field
+//        holder.doctorName.setOnClickListener{
+//            clickHandler?.invoke(dataSet[position])
+//        }
+        holder.bind(dataSet[position],clickHandler)
     }
     override fun getItemCount(): Int = dataSet.size
 }
